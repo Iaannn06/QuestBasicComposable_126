47e9a74a732e758af1445ec75415d20880befe76
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-10-16 16:32:58 +0700

    menambahkan isi dari kolom Row1 dan Row2 beserta isinya

--- app/src/main/java/com/example/tataletak/TataLetak_126.kt
+++ app/src/main/java/com/example/tataletak/TataLetak_126.kt
@@ -69,12 +69,18 @@ fun TataLetakColumnRow(modifier: Modifier) {
         //Baris1
         Row(modifier = modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceEvenly) {
-            Text(text = "Box 1")
-            Text(text = "Column 1")
-            Text(text = "Row 1")
-            Text(text = "Box 2")
-            Text(text = "Column 2")
+            Text(text = "Komponen1Baris1")
+            Text(text = "Komponenen2Baris1")
+            Text(text = "Komponen3Baris1")
 
+        }
+
+        //Baris2
+        Row(modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly) {
+            Text(text = "Komponen1Baris2")
+            Text(text = "Komponenen2Baris2")
+            Text(text = "Komponen3Baris2")
         }
     }
 }
