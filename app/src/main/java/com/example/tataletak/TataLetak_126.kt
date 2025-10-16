10d0b590acc980b40c15351b82a75905d142b30a
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-10-16 16:36:37 +0700

    menambahkan isi dari Row2

--- app/src/main/java/com/example/tataletak/TataLetak_126.kt
+++ app/src/main/java/com/example/tataletak/TataLetak_126.kt
@@ -98,6 +98,13 @@ fun TataLetakRowColumn(modifier: Modifier) {
             Text(text = "Komponen3Baris1")
 
         }
+        //Baris2
+        Row(modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly) {
+            Text(text = "Komponen1Baris2")
+            Text(text = "Komponenen2Baris2")
+            Text(text = "Komponen3Baris2")
+        }
 
     }
 
